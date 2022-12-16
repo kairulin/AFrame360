c71bb68190638e21e352e5d7ed0908e19735898a
LinKarRu <wl03352521@gmail.com>
2022-12-16 11:56:33 +0800

    add info in scene

--- change-site.js
+++ change-site.js
@@ -1,4 +1,3 @@
-
 AFRAME.registerComponent("change-site", {
   schema: {
     img: { type: "string" },
@@ -17,13 +16,19 @@ AFRAME.registerComponent("call-info", {
   schema: {
     data: { type: "string" },
     index: { type: "number" },
+    btn: { type: "string" },
   },
   init: function () {
     var data = this.data;
     var el = this.el;
     // 點擊資訊
+    var openInfo = document.querySelector(data.data);
+    var infoBtn = document.querySelector(data.btn);
+
     el.addEventListener("click", function () {
-      infoClick(data.data,data.index);
+      openInfo.setAttribute("visible", true);
+      infoBtn.classList.add("clickable");
+      // infoClick(data.data,data.index);
     });
     // 滑入放大
     el.addEventListener("mouseenter", function () {
@@ -32,6 +37,23 @@ AFRAME.registerComponent("call-info", {
     //離開縮小
     el.addEventListener("mouseleave", function () {
       itemLeave();
+    });
+  },
+});
+
+AFRAME.registerComponent("close-info-btn", {
+  schema: {
+    data: { type: "string" },
+  },
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+
+    var infos = document.querySelectorAll("#infomation");
+    var btn = document.querySelector(data.data);
+    el.addEventListener("click", function () {
+      infos.forEach(info=>info.setAttribute("visible", false));
+      btn.classList.remove("clickable");
     });
   },
 });
